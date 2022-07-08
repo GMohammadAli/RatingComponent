d4b5f9732f35af427741e29c532eba373cdac94d
GMohammadAli <gulammohammadali@gmail.com>
2022-07-08 13:56:22 +0530

    Template Complete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Form from "./components/Form";
+// import SubmitState from "./components/SubmitState";
 
 function App() {
   return (
     <div className="body">
       <div className="container">
+        {/* <Router>
+          <Routes>
+            <Route path="/" element={<Form />}/>
+            <Route path="/submitted" element={<SubmitState />}/>
+          </Routes>
+        </Router> */}
         <Form />
       </div>
     </div>
---
+++ src/components/SubmitState.js
@@ -0,0 +1,17 @@
+
+
+function SubmitState() {
+  return (
+     <div className="card-two hidden">
+      <div className="img"><img src="./images/illustration-thank-you.svg" alt="thanks" /></div>
+      <div className="selected-rating"></div>
+      <div className="text">
+        <h1>Thank you!</h1>
+        <p>We appreciate you taking the time to give a rating. If you ever need more support, don’t hesitate to get in
+          touch!</p>
+      </div>
+    </div>
+  )
+}
+
+export default SubmitState
